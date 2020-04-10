651919390086d2818b3c3ce19ce0181641712192
sphung11 <stephanie@axcessiom.ca>
2020-04-09 21:50:27 -0400

    Demo complete.

---
+++ Cars/Assets/CameraManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject focus;
+    public float distance = 5f;
+    public float height = 1.25f;
+    public float dampening = 0.5f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position, 
+            focus.transform.position + focus.transform.TransformDirection(new Vector3(0f, height, -distance)), 
+            Time.deltaTime * dampening
+        );
+
+        transform.LookAt(focus.transform);
+    }
+}
---
+++ Cars/Assets/CarController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager))]
+[RequireComponent(typeof(Rigidbody))]
+public class CarController : MonoBehaviour
+{
+    public InputManager inputManager;
+    public List<WheelCollider> throttleWheels;
+    public List<WheelCollider> steeringWheels;
+    public float strengthCoefficient = 7500f;
+    public float maxTurn = 20f;
+    public Transform centerOfMass;
+    public Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputManager = GetComponent<InputManager>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        if (centerOfMass)
+        {
+            rigidbody.centerOfMass = centerOfMass.position;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        foreach(WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strengthCoefficient * Time.deltaTime * inputManager.throttle;
+        }
+
+        foreach(WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = maxTurn * inputManager.steer;
+        }
+    }
+}
